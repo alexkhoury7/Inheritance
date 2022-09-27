19f999f0f60a7081f26c77f3de1a82059c079400
Alex <alex@test.com>
2022-09-27 12:34:25 -0500

    class

--- f_polymorphism_demo.py
+++ f_polymorphism_demo.py
@@ -27,5 +27,17 @@ def main():
     cat.show_species()
     cat.make_sound()
 
+    show_mammal_info(mammal)
+    show_mammal_info(dog)
+    show_mammal_info(cat)
+    show_mammal_info('Bird')
+
+def show_mammal_info(creature):
+    if isinstance(creature, animals.Mammal):
+        creature.show_species()
+        creature.make_sound()
+    else:
+        print('this is not a mammal')
+
 # Call the main function.
 main()
